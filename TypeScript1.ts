cbbda4effd69c2fe7dc58bbbd009ed24b64fa29d
sd1026 <pintudixit73@gmail.com>
2020-06-03 16:26:27 +0530

    final commit

---
+++ TypeScript1.ts
@@ -0,0 +1,12 @@
+declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+export class faq extends React.Component {
+    render() {
+        return ();
+    }
+}
+
+ReactDOM.render(<faq/>, document.getElementById('root1'));
